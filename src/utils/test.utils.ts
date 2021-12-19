d43a2ddb11d373b866570bc77d696e9ae678cebd
nilushiAyeshika <51369618+nilushiAyeshika@users.noreply.github.com>
2021-12-19 10:09:48 +0530

    Delete test.utils.ts

--- src/utils/test.utils.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @function findByTestAttr
- * @param {*} wrapper
- * @param {*} val
- * @returns {ShallowWrapper}
- */
-export const findByTestAttr = (wrapper: any, val: any) => {
-  return wrapper.find(`[data-test='${val}']`)
-}
